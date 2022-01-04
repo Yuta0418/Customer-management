8893c9f9acb3574b39656e636de1536a650eb335
Yuta0418 <yuta19920418@gmail.com>
2022-01-04 17:22:28 +0900

    first commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
+     */
+    public function __invoke(Request $request)
+    {
+        $user = auth()->user();
+        $this->authorize('viewAny', $user);  // Policy をチェック
+        $users = \App\Models\User::get(); // 社員一覧を取得
+        return view('users.index', compact('users'));
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', [\App\Http\Controllers\TopPageController::class, 'top_page'])->name('top_page');
+Route::get('/users', \App\Http\Controllers\UserController::class)->name('社員一覧')->middleware('auth');
+Route::get('/roles', \App\Http\Controllers\RoleController::class)->name('ロール一覧')->middleware('auth');
+Route::resource('/customers', \App\Http\Controllers\CustomerController::class)->middleware('auth');
+Route::get('customer_search', 'CustomerSearchController@index')->middleware('auth');
+
+
+
+
